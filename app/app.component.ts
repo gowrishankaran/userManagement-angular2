f6c2bd959b30defc5de53857633fdef7a7eee564
gowrishankaran <gowrishankaran008@gmail.com>
2017-05-07 19:06:45 +0530

    Route Congigured (SPA)

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
 	selector:'my-app',
-	template:'<h1>User Management</h1><router-outlet></router-outlet>'
+	templateUrl:'./app/app.component.html'
 })
 export class AppComponent {
 
